c618c86bef80ea9c0ca7400e9badf1cf910ef51b
Minentle Mvunyiswa <102150619+Minentle2301@users.noreply.github.com>
2024-08-02 17:13:45 +0200

    updated my add to cart function

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+const appSettings = {
+    databaseURL: "https://mobile-app-48c8f-default-rtdb.firebaseio.com/" // Replace with my actual Firebase Realtime Database URL
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+    let inputValue = inputFieldEl.value.trim();
     
-    console.log(inputValue)
-})
+    if (inputValue !== "") {
+        push(shoppingListInDB, inputValue);
+        clearInputEl();
+    }
+});
+
+onValue(shoppingListInDB, function(snapshot) {    
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val());
+    
+        clearShoppingListEl();
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i];
+            appendItemToShoppingListEl(currentItem);
+        } 
+    } else {
+        shoppingListEl.innerHTML = "No items in list... yet";
+    }
+});
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = "";
+}
+
+function clearInputEl() {
+    inputFieldEl.value = "";
+}
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0];
+    let itemValue = item[1];
+    
+    let newEl = document.createElement("li");
+    
+    newEl.textContent = itemValue;
+    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+        remove(exactLocationOfItemInDB);
+    });
+    
+    shoppingListEl.append(newEl);
+}
